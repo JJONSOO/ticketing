cf2a44423ddc727465363e4b3dbdbab2f4bfed23
JJONSOO <pjs970616@naver.com>
2023-10-11 20:56:02 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/pilot/TicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
