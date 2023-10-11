dab4e455fa73acb31211c840cea85c85d75e81ca
JJONSOO <pjs970616@naver.com>
2023-10-11 21:02:53 +0900

    feat: Reservation 생성

---
+++ src/main/java/com/pilot/reservation/Reservation.java
@@ -0,0 +1,20 @@
+package com.pilot.reservation;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Reservation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+
+}
